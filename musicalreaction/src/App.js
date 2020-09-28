3091988850f2dad763929f663f6fc897ed31226f
Daniel Christine <danieljameschristine@gmail.com>
2020-09-28 13:28:30 -0500

    Axios consumption command added

--- musicalreaction/src/App.js
+++ musicalreaction/src/App.js
@@ -1,26 +1,25 @@
+import axios from 'axios';
 import React from 'react';
-import logo from './logo.svg';
+import { AppState, StyleSheet, Text, View } from "react-native";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
     </div>
   );
 }
+
+
+useEffect(() => {
+  setAppState({ loading: true });
+  const apiUrl = 'http://devcodecampmusiclibrary.com/';
+  axios.get(apiUrl).then((repos) => {
+    const allRepos = repos.data;
+    setAppState({ loading: false, repos: allRepos });
+  });
+}, [setAppState]);
 
 export default App;
